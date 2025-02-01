604c08a65e8bc4a19919030fd4275a3e2e67aa17
Gihad Abdelhamid <ghgalal@gmail.com>
2025-02-01 12:06:54 -0500

    Add files via upload

---
+++ cpd_vars.sh
@@ -0,0 +1,107 @@
+#===============================================================================
+# Cloud Pak for Data installation variables 5.1
+#===============================================================================
+
+# ------------------------------------------------------------------------------
+# Client workstation 
+# ------------------------------------------------------------------------------
+# Set the following variables if you want to override the default behavior of the Cloud Pak for Data CLI.
+#
+# To export these variables, you must uncomment each command in this section.
+
+# export CPD_CLI_MANAGE_WORKSPACE=<enter a fully qualified directory>
+# export OLM_UTILS_LAUNCH_ARGS=<enter launch arguments>
+
+
+# ------------------------------------------------------------------------------
+# Cluster
+# ------------------------------------------------------------------------------
+
+export OCP_URL=https://api.679159d7e4b091eaa609cf5b.ocp.techzone.ibm.com:6443
+export OPENSHIFT_TYPE="self-managed"
+export IMAGE_ARCH="amd64"
+export OCP_USERNAME=kubeadmin
+export OCP_PASSWORD=wcsGc-3wvnV-uHVLS-FQgog
+# export OCP_TOKEN=<enter your token>
+export SERVER_ARGUMENTS="--server=${OCP_URL}"
+export LOGIN_ARGUMENTS="--username=${OCP_USERNAME} --password=${OCP_PASSWORD}"
+# export LOGIN_ARGUMENTS="--token=${OCP_TOKEN}"
+export CPDM_OC_LOGIN="cpd-cli manage login-to-ocp ${SERVER_ARGUMENTS} ${LOGIN_ARGUMENTS}"
+export OC_LOGIN="oc login ${SERVER_ARGUMENTS} ${LOGIN_ARGUMENTS}"
+
+
+# ------------------------------------------------------------------------------
+# Proxy server
+# ------------------------------------------------------------------------------
+
+# export PROXY_HOST=<enter your proxy server hostname>
+# export PROXY_PORT=<enter your proxy server port number>
+# export PROXY_USER=<enter your proxy server username>
+# export PROXY_PASSWORD=<enter your proxy server password>
+
+
+# ------------------------------------------------------------------------------
+# Projects
+# ------------------------------------------------------------------------------
+
+export PROJECT_CERT_MANAGER=ibm-cert-manager
+export PROJECT_LICENSE_SERVICE=cpd-licensing
+export PROJECT_SCHEDULING_SERVICE=cpd-scheduler
+export PROJECT_IBM_EVENTS=ibm-knative-events
+# export PROJECT_PRIVILEGED_MONITORING_SERVICE=<enter your privileged monitoring service project>
+export PROJECT_CPD_INST_OPERATORS=cpd-operators
+export PROJECT_CPD_INST_OPERANDS=cpd-operands
+# export PROJECT_CPD_INSTANCE_TETHERED=<enter your tethered project>
+# export PROJECT_CPD_INSTANCE_TETHERED_LIST=<a comma-separated list of tethered projects>
+
+
+
+# ------------------------------------------------------------------------------
+# Storage
+# ------------------------------------------------------------------------------
+
+export STG_CLASS_BLOCK=ocs-storagecluster-ceph-rbd
+export STG_CLASS_FILE=ocs-storagecluster-cephfs
+
+# ------------------------------------------------------------------------------
+# IBM Entitled Registry
+# ------------------------------------------------------------------------------
+
+export IBM_ENTITLEMENT_KEY=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJJQk0gTWFya2V0cGxhY2UiLCJpYXQiOjE3MzczOTYwMDAsImp0aSI6IjM0NTQ1ODI4NGU1MTQ4M2Q5NjE2MWI3YTdlYjcxMDAyIn0.UvA8K--UXFZ4u7_ha_IeSN-p1Op_EdyI5PJxvgrSJIY
+
+
+# ------------------------------------------------------------------------------
+# Private container registry
+# ------------------------------------------------------------------------------
+# Set the following variables if you mirror images to a private container registry.
+#
+# To export these variables, you must uncomment each command in this section.
+
+# export PRIVATE_REGISTRY_LOCATION=<enter the location of your private container registry>
+# export PRIVATE_REGISTRY_PUSH_USER=<enter the username of a user that can push to the registry>
+# export PRIVATE_REGISTRY_PUSH_PASSWORD=<enter the password of the user that can push to the registry>
+# export PRIVATE_REGISTRY_PULL_USER=<enter the username of a user that can pull from the registry>
+# export PRIVATE_REGISTRY_PULL_PASSWORD=<enter the password of the user that can pull from the registry>
+
+
+# ------------------------------------------------------------------------------
+# Cloud Pak for Data version
+# ------------------------------------------------------------------------------
+
+export VERSION=5.1.0
+
+
+# ------------------------------------------------------------------------------
+# Components
+# ------------------------------------------------------------------------------
+
+export COMPONENTS=ibm-cert-manager,ibm-licensing,scheduler,cpfs,cpd_platform
+# export COMPONENTS_TO_SKIP=<component-ID-1>,<component-ID-2>
+
+
+# ------------------------------------------------------------------------------
+# watsonx Orchestrate
+# ------------------------------------------------------------------------------
+export PROJECT_IBM_APP_CONNECT=appconnect
+export AC_CASE_VERSION=12.5.0
+export AC_CHANNEL_VERSION=v12.5
